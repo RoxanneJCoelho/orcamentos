1620d8ac15b5cf14d4e858c8a25c29043cca3f5c
Roxanne Coelho <163065171+RoxanneJCoelho@users.noreply.github.com>
2025-08-21 14:58:00 +0100

    update database budget

---
+++ database/migrations/2025_08_21_135630_create_option_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('option', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('budget_id');
+            $table->foreign('budget_id')->references('id')->on('budget');
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('service');
+            $table->integer('qtd');
+            $table->double('valor', 8, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('option');
+    }
+};
