40860e4230b4b41e978a61fcbec9e07211d066ac
roxie <1003rjc@gmail.com>
2025-09-15 09:43:03 +0100

    last update

--- resources/views/pdf/orcamento.blade.php
+++ resources/views/pdf/orcamento.blade.php
@@ -1,31 +1,26 @@
-@extends('layouts.master')
+<h1>Orçamento</h1>
+<table>
+    <thead>
+        <tr>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Preço Sem Desconto</th>
+            <th>Desconto</th>
+            <th>Valor com Desconto</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($tabela as $item)
+        <tr>
+            <td>{{ $item['descricao'] }}</td>
+            <td>{{ $item['quantidade'] }}</td>
+            <td>{{ number_format($item['precoSemDesconto'], 2) }}€</td>
+            <td>{{ $item['desconto'] }}%</td>
+            <td>{{ number_format($item['valorComDesconto'], 2) }}€</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 
-@section('content')
-<div id="barraTotal" class="mt-4 mb-5 p-3 bg-light border rounded">
-            <h5>Serviços Selecionados</h5>
-            <div class="table-responsive">
-                <table class="table table-sm table-hover" id="tabelaSelecionados">
-                    <thead class="table-light">
-                        <tr>
-                            <th>Serviço</th>
-                            <th>Quantidade</th>
-                            <th>Preço S/Desconto</th>
-                            <th>Desconto</th>
-                            <th>Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <td></td>
-                    <td>quantidade</td>
-                    <td>precoSemDesconto€</td>
-                    <td>desconto%</td>
-                    <td>valorComDesconto€</td>
-                    </tbody>
-                </table>
-            </div>
-            <h4>Total: <span id="precoTotal">{{$code}}</span>€</h4>
-        </div>
-@endsection
-@section('scripts')
-<script src="{{ asset('assets/js/budget.js') }}"></script>
-@endsection
+<p>Total: {{ number_format($precoTotal, 2) }}€</p>
+
